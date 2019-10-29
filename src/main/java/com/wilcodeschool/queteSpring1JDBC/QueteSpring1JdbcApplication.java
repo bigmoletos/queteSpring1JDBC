cb13699c5159bf5bf694900ec480b375a3e975be
bigmoletos <bigmoletos@yopmail.com>
2019-10-29 15:55:36 +0100

    first commit

---
+++ src/main/java/com/wilcodeschool/queteSpring1JDBC/QueteSpring1JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.wilcodeschool.queteSpring1JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteSpring1JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteSpring1JdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilcodeschool/queteSpring1JDBC/QueteSpring1JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilcodeschool.queteSpring1JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueteSpring1JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
